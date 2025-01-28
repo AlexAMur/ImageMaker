5c3d94115ace33a8f06d1fac8cc94934603eab75
Alex <muratov777sa@gmail.com>
2025-01-29 00:30:26 +0300

    net_function_2day

--- app/src/main/java/com/example/imagemaker/netClass.kt
+++ app/src/main/java/com/example/imagemaker/netClass.kt
@@ -1,16 +1,22 @@
 package com.example.imagemaker
 
 import android.content.Context
+import android.content.Intent
 import android.net.ConnectivityManager
+import android.net.InetAddresses
+import android.net.wifi.WifiManager
 import androidx.core.content.ContextCompat.getSystemService
+import java.net.Inet4Address
 
 
 class netClass {
 
     fun getIP4Address(context: Context){
-        val connectivityManager = getSystemService(context, ConnectivityManager::class.java)
-        val currentNet = connectivityManager.getActiveNetwork()
-       val ipv4Address = getIP4Address()
+        val connectivityManager = context.getSystemService(Context.WIFI_SERVICE) as WifiManager
+
+        val currentNet = connectivityManager.scanResults
+       val ipv4Address =
+        return ipv4Address
     }
     fun checkWIFI(){
 
